168e30d21aeecc39d274262ec934b4c45441eb82
TestLeafPages <earth@testleaf.com>
2022-12-10 18:52:24 +0530

    Add files via upload

---
+++ week7/day1/CreateLead.java
@@ -0,0 +1,42 @@
+package week7.day1;
+
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import week5.day1.testcase.BaseClass;
+
+public class CreateLead extends ProjectSpecificMethod{
+	@BeforeTest
+	public void openDb() {
+		 excelfile="CreateLead";
+	}
+	
+	@Test(dataProvider = "fetchData")
+
+	public  void runCreateLead(String cname,String fname,String lname,String phnum) {
+		
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(cname);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(fname);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lname);
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys(phnum);
+		driver.findElement(By.name("submitButton")).click();
+		
+} 
+}
+
+
+
+
+
+
---
+++ week7/day1/ReadExcel.java
@@ -0,0 +1,44 @@
+package week7.day1;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+
+	public static  String[][] readdata(String fileName) throws IOException {
+		       //set the path
+				XSSFWorkbook wbook=new XSSFWorkbook("Data/"+fileName+".xlsx");
+				//Open the sheet
+				XSSFSheet sheet = wbook.getSheetAt(0);//Read the first sheet index start from 0
+				
+				//Row count
+				
+				int rowCount = sheet.getLastRowNum();//ctrl+2
+				//column count
+				short columnCount = sheet.getRow(0).getLastCellNum();//ctrl+2
+				//data
+				String [][] data=new String[rowCount][columnCount];
+				//get row value outer loop
+				for(int i=1;i<=rowCount;i++) {
+				XSSFRow row = sheet.getRow(i);//ctrl+2+l
+				//column count using inner loop
+				for (int j = 0; j<columnCount; j++) {
+				XSSFCell cell = row.getCell(j);//ctrl+2+l
+		        System.out.println(cell.getStringCellValue());
+		        data[i-1][j]= cell.getStringCellValue();//0 0
+		        
+				}
+				
+				}
+				//to close the workbook
+				wbook.close();
+				return data;
+				
+
+	}
+
+}
